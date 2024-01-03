3cc698a53dfd0f482e765aeb2863625383b8bc90
Haskarchik <vovakryt24@gmail.com>
2024-01-03 23:34:39 +0200

    FIX

--- src/UI/telegram/telegrem.php
+++ src/UI/telegram/telegrem.php
@@ -9,13 +9,13 @@ $arr = array(
     "Номер телефону =>"=>$message,
     "Email =>"=>$email,
 );
-for ($arr as $key =>$value) { 
-    $txt = "<b>".$key."</b ".$value."%0A";
+foreach($arr as $key => $value) { 
+    $txt .= "<b>".$key."</b ".$value."%0A";
 
 }
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 if($sendToTelegram){
-    header('Location : success.html');
+    echo('Location : success.html');
 }else{
     echo "Error"
 }
