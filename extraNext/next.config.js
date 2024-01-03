9e6d48392086d86fee77f9b1d04fc0e1db0647d2
ExtraPlatinumShop <155564592+ExtraPlatinumShop@users.noreply.github.com>
2024-01-03 18:37:08 +0200

    Delete extraNext directory

--- extraNext/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = {
-  i18n: {
-    locales: ['ua', 'en'],
-    defaultLocale: 'ua',
-  },}
-  
-module.exports = nextConfig
--- extraNext/src/components/Why-we-seaction/WhyWe.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function WhyWe() {
-  return (
-    <div>WhyWe</div>
-  )
-}
