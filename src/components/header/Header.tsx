d2c8fc1c76872cf8d969859045b709f97051884c
Haskarchik <vovakryt24@gmail.com>
2024-12-15 17:32:58 +0200

    add to publick

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -62,7 +62,7 @@ export default function Header() {
               <Link href="/About-us">{t("Header_About_Us")}</Link>
             </li>
             <li>
-              <Link href="/Cart">{t("Header_Basket")}</Link>
+              <Link href="/inFeature">{t("Header_Basket")}</Link>
             </li>{" "}
             <li>
               <Link href="/Cooperation">{t("Header_Сooperation")}</Link>
