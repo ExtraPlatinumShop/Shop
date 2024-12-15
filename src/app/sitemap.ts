789c29248514c3e50683f0d49d059156942e848f
Haskarchik <vovakryt24@gmail.com>
2024-12-15 18:33:46 +0200

    modefied sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,40 +3,37 @@ import { ref, get } from "firebase/database";
 import { db } from "@/UI/firebase/data";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  // Отримуємо всі ID продуктів з Firebase
   const productsRef = ref(db, 'products');
   const snapshot = await get(productsRef);
   const products = snapshot.val() || {};
   
-  // Базові URL
   const baseUrls = [
     {
       url: 'https://extra-platinum.com.ua',
       lastModified: new Date(),
-      changeFrequency: 'daily',
+      changeFrequency: 'daily' as const,
       priority: 1,
     },
     {
       url: 'https://extra-platinum.com.ua/About-us',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: 'monthly' as const,
       priority: 0.8,
     },
     {
       url: 'https://extra-platinum.com.ua/Cart',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: 'monthly' as const,
       priority: 0.5,
     },
     {
       url: 'https://extra-platinum.com.ua/Cooperation',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: 'monthly' as const,
       priority: 0.8,
     },
   ]
 
-  // Додаємо URL продуктів
   const productUrls = Object.keys(products).map((productId) => ({
     url: `https://extra-platinum.com.ua/Product/${productId}`,
     lastModified: new Date(),
