e734d9bf4a8968cdecece1fdd7fc0aa5b520af3d
Haskarchik <vovakryt24@gmail.com>
2025-01-10 16:17:09 +0200

    Refactor HomeScreen component and update Next.js configuration: Removed localization settings from next.config.js and added remote image patterns. Updated HomeScreen to use remote images with lazy loading for better performance and optimized rendering.

--- src/components/HomeScreen/HomeScreen.tsx
+++ src/components/HomeScreen/HomeScreen.tsx
@@ -17,18 +17,27 @@ export default function HomeScreen() {
         <div className={style.homeScreen_row}>
           <Image
             className={style.leftProduct}
-            src={leftProduct}
+            src="https://firebasestorage.googleapis.com/v0/b/extra-d6506.appspot.com/o/Little_soap%2Flitl-soap-aloe.webp?alt=media&token=3cb804c8-4753-40a5-98ad-cc4c784c40a4"
             alt="Product image"
+            loading="lazy"
+            width={100}
+            height={100}
           ></Image>
           <Image
+            loading="lazy"
             className={style.rightProduct}
-            src={rightProduct}
+            src="https://firebasestorage.googleapis.com/v0/b/extra-d6506.appspot.com/o/%D0%A1onditioner%2Fopolisk-yellow.webp?alt=media&token=d64e55f8-63ae-40bf-bdef-acc5b712c1bf"
             alt="Product image"
+            width={100}
+            height={100}
           ></Image>
           <Image
+            loading="lazy"
             className={style.centerProduct}
-            src={centerProduct}
+            src="https://firebasestorage.googleapis.com/v0/b/extra-d6506.appspot.com/o/Toilet_gel%2Ftoilet.webp?alt=media&token=bf7ab299-14c0-425f-bb26-5fe719cae9bc"
             alt="Product image"
+            width={100}
+            height={100}
           ></Image>
           <Image className={style.aloe} src={aloe} alt="Aloe image"></Image>
           <div className={style.label}>
