3638caad998d6b23bd484ca0eacf3899a6e6eece
haskarchik <vovakryt24@gmail.com>
2025-04-18 00:00:36 +0300

    Refactor email sending functionality: Update API endpoint to Telegram for sending messages, enhance request structure with headers, and implement message formatting for improved readability and detail in notifications.

--- src/UI/mail/send-email.ts
+++ src/UI/mail/send-email.ts
@@ -1,12 +1,18 @@
 import { FormData } from "@/app/types/form";
 
-
 export function sendEmail(data: FormData) {
-  const apiEndpoint = '@/app/api/email/route.ts';
+  const apiEndpoint = 'https://api.telegram.org/bot6855648363:AAFRbWZ5tL_ESAb31ufDnwqHnNGOVzXRCWE/sendMessage';
 
   fetch(apiEndpoint, {
     method: 'POST',
-    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      chat_id: -1002041235960,
+      text: formatMessage(data),
+      parse_mode: 'HTML'
+    }),
   })
     .then((res) => res.json())
     .then((response) => {
@@ -15,4 +21,22 @@ export function sendEmail(data: FormData) {
     .catch((err) => {
       alert(err);
     });
+}
+
+function formatMessage(data: FormData): string {
+  return `
+🛒 НОВЕ ЗАМОВЛЕННЯ
+────────────────────
+• Дата: <b>${new Date().toLocaleDateString()}</b>
+• Ім'я: <b>${data.name}</b>
+• Телефон: <b>${data.phone}</b>
+• Email: <b>${data.email}</b>
+• Місто: <b>${data.city}</b>
+• Доставка: <b>${data.delivery}</b>
+• Оплата: <b>${data.payment}</b>
+
+📝 КОМЕНТАР:
+<i>${data.comment || 'Немає коментаря'}</i>
+────────────────────
+  `;
 }
